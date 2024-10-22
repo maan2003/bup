e11c6d1a57519f288e206fee6a694bd955c71b4c
maan2003 <manmeetmann2003@gmail.com>
2024-10-22 15:57:06 +0530

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod blober;
 pub mod hash_value;
+pub mod server_store;
 
 #[cfg(test)]
 mod tests;
---
+++ src/server_store.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+use crate::hash_value::HashValue;
+
+#[async_trait::async_trait]
+pub trait Storage {
+    async fn has(&self, hash: &[u8]) -> anyhow::Result<bool>;
+    async fn put(&self, key: &[u8], value: &[u8]) -> anyhow::Result<()>;
+    async fn get(&self, key: &[u8]) -> anyhow::Result<Vec<u8>>;
+}
+pub struct InMemoryStorage {
+    data: Arc<Mutex<HashMap<Vec<u8>, Vec<u8>>>>,
+}
+
+impl InMemoryStorage {
+    pub fn new() -> Self {
+        InMemoryStorage {
+            data: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
+#[async_trait::async_trait]
+impl Storage for InMemoryStorage {
+    async fn has(&self, hash: &[u8]) -> anyhow::Result<bool> {
+        Ok(self.data.lock().await.contains_key(hash))
+    }
+
+    async fn put(&self, key: &[u8], value: &[u8]) -> anyhow::Result<()> {
+        self.data
+            .lock()
+            .unwrap()
+            .await
+            .insert(key.to_vec(), value.to_vec());
+        Ok(())
+    }
+
+    async fn get(&self, key: &[u8]) -> anyhow::Result<Vec<u8>> {
+        self.data
+            .lock()
+            .await
+            .get(key)
+            .cloned()
+            .ok_or_else(|| anyhow::anyhow!("Key not found"))
+    }
+}
